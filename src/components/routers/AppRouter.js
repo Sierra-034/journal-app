1edec6b2c43239849ffadbc2317b5aac74e93fa1
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-08-09 10:16:15 -0500

    219: Implementando rutas principales y rutas hijas

---
+++ src/components/routers/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { JournalScreen } from '../journal/JournalScreen';
+import { AuthRouter } from './AuthRouter';
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/auth" component={AuthRouter} />
+                    <Route exact path="/" component={JournalScreen} />
+
+                    <Redirect to="/auth/login" />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+};
+ 
