caea6f3af69d901b929da3a3b95bfece5756e4b4
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-08-22 19:17:09 -0500

    228: Estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
+            <div className='notes__content'>
+                <input
+                    type='text'
+                    placeholder='Some awsome title'
+                    className='notes__title-input'
+                    autoComplete='off'
+                />
+
+                <textarea
+                    placeholder='What happened today?'
+                    className='notes__text-area'
+                ></textarea>
+
+                <div className='notes__image'>
+                    <img
+                        src='https://picsum.photos/200/300'
+                        alt='note_image'
+                    />
+                </div>
+            </div>
         </div>
     );
 };
