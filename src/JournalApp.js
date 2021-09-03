a037a5dd91cd6021c9369d7bc104d60f65b6d0ab
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-03 10:45:38 -0500

    236: Configurando Redux en nuestra aplicación

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+
 import { AppRouter } from './components/routers/AppRouter';
 
 export const JournalApp = () => {
     return (
-        <AppRouter />
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
     )
 };
